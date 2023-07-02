f5d3f752411d6806b62f5ff6c1864323eef79164
Chris Byrne <chris.byrne@gmail.com>
2023-07-02 14:59:15 +0100

    Update weather.js

--- services/weather.js
+++ services/weather.js
@@ -10,7 +10,7 @@ export const getWeatherData = async (location) => {
     }
 
     const res = await fetch(
-        `http://api.weatherapi.com/v1/current.json?key=${API_KEY}&q=${location}&aqi=no`,
+        `http://api.weatherapi.com/v1/forecast.json?key=${API_KEY}&q=${location}&aqi=no`,
     );
     return res.json();
 }
